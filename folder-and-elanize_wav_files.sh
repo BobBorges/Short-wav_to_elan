2babc62b6e5ca7542490bf0094fa120fa41385d8
bob <robert.d.borges@gmail.com>
2019-11-06 19:55:22 +0100

    added support for specifying target directories by.txt file, allowing for operating on multiple directories. Also added fancy options menu.

--- folder-and-elanize_wav_files.sh
+++ folder-and-elanize_wav_files.sh
@@ -14,18 +14,7 @@
 #
 # DEPENDENCIES: soxi, tidy
 #
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Enter the directory you want to 'folderize' and 'elanize'."
-echo "Hint: use absolute paths"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo ""
-echo "Directory:" && read working_dir
-echo ""
-echo "The following files will be put into sub directories:"
-ls $working_dir/*.wav
-sleep 1
-echo "If there's a .txt file with the same name as a .wav, it will be put in the new sub directory too."
-sleep 3
+F_and_E_ize () {
 for file in $working_dir/*.wav
 do
     if [[ -f $file ]]; then
@@ -44,10 +33,10 @@ do
         if [[ -f $file ]]; then
             file_bare=${file%.wav}
             file_name=${file_bare##*/}
-            sec_dur=$(soxi -D $file)					        # this gets the durration of the audio file in seconds
-            ms_dur_f=$(echo "${sec_dur}*1000"|bc)				# converts seconds to ms
-            ms_dur=${ms_dur_f%.*}						        # and removes the decimal and post decimal val
-            if [[ -f "$sub_dir/$file_name.txt" ]]; then			# checks if there's a .txt file by the same name as the .wav
+            sec_dur=$(soxi -D $file)				# this gets the durration of the audio file in seconds
+            ms_dur_f=$(echo "${sec_dur}*1000"|bc)		# converts seconds to ms
+            ms_dur=${ms_dur_f%.*}				# and removes the decimal and post decimal val
+            if [[ -f "$sub_dir/$file_name.txt" ]]; then		# checks if there's a .txt file by the same name as the .wav
                 transcription=$( cat "$sub_dir/$file_name.txt")	# assigns contents of .txt to a var
             fi
             # this is a non-pretty version of an empty elan file, filling in vars from the .wav and .txt
@@ -60,6 +49,97 @@ do
     done
     fi
 done
-echo "~~~~"
-echo "DONE"
-echo "~~~~"
+echo -e "\e[31m~~~~"
+echo -e "DONE"
+echo -e "~~~~\e[0m"
+} 
+one_dir () {
+	echo "Please enter the directory you want to operate on."
+	echo "HINT: Use an absolute path."	
+	read working_dir
+	F_and_E_ize
+}
+two_dir () {
+	echo "Please enter a .txt file listing the directories you want to operate on."
+	echo "HINT: Use absoute paths to the file AND in the file."
+	read file_list
+	while read line; do
+		working_dir=$line
+		F_and_E_ize	
+	done < $file_list
+}
+help_msg () {
+	echo -e "\e[33mThis is a script to put files .wav files within a directory into individual"
+	echo -e "subdirectories that are also named the same way as the files. If there is a"
+	echo -e ".txt file with the same name as the .wav file, it will also be moved to the"
+	echo -e "new subdirectory."
+	echo -e ""
+	echo -e "Once the new subdirectory is created and files added, the script will"
+	echo -e "create an elan file with associated with the .wav in its subdirectory."
+	echo -e "The .eaf file will have one tier and an anotation that occupies the entire"
+	echo -e "durration of the .wav file. If there is a .txt file in the directory with"
+	echo -e "the same name as the .wav file, the script will assume it is transcription"
+	echo -e "and add the contents of the .txt to the annotation."
+	echo -e ""
+	echo -e "Required DEPENDENCIES: soxi, tidy"
+	echo -e ""
+	echo -e "If tou choose to operate on one directory (Option 1), you must enter an"
+	echo -e "absolute path to that directory. Sommething like:\e[0m"
+	echo -e ""
+	echo -e "    /home/user/target_directory"
+	echo -e ""
+	echo -e "\e[33mIf you choose to operate on multiple directories, you must provide"
+	echo -e "a .txt file as input. An absolute path should be entered to that fiel, and"
+	echo -e "its contents should be a line by line list of directories to be operated"
+	echo -e "on by the script."
+	echo -e ""
+	echo -e "OK?"
+	echo -e ""
+	sleep 1
+	echo -e "\e[31mNow repeating the options:"
+	echo -e "\e[33m"
+	PS3=""
+	sleep 1
+	echo "x" | select opt in "${options[@]}"; do break;done;  # $REPLY";;
+	export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+}
+echo -e "\e[31m |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+echo -e " | --- Welcome to folderize and elanize! --- |"
+echo -e " |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+sleep 1
+echo ""
+echo -e "How many directories to you want to operate on?\e[33m"
+echo ""
+sleep 1
+
+COLUMNS=12
+export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+options=("Work on a single directory" "Work on multiple directories" "Get help" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"Work on a single directory")
+			echo "You chose to work on ONE directory."
+			one_dir
+			break		
+			;;
+		"Work on multiple directories")
+			echo "You chose to work on MULTIPLE directories."
+			two_dir
+			break
+			;;
+		"Get help")
+			echo "So, you need help? Here it is:"
+			help_msg
+			;;
+		"Quit")
+			break
+			;;
+		*) 
+			PS3=""
+			echo -e "\e[1mInvalid option!!\e[0m Try again.\e[33m"
+			sleep 1
+			echo "x" | select opt in "${options[@]}"; do break;done;  # $REPLY";;
+			export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+	esac
+done
