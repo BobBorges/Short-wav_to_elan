24fd644ce4e5d155bf1e7276f460926381d1b1d3
bob <robert.d.borges@gmail.com>
2019-11-06 21:32:06 +0100

    added gpl license

--- folderize_wav_files.sh
+++ folderize_wav_files.sh
@@ -4,7 +4,23 @@
 # subdirectories that are also named the same way as the files. If there is a
 # .txt file with the same name as the .wav file, it will also be moved to the
 # new subdirectory.
-
+#
+#    ###########
+#    # LICENSE #
+#    ###########
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 FOLDERIZE () {
     for file in $working_dir/*.wav
     do
