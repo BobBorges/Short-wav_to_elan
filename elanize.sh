9664331a6b6a99327f5bacc8d869dfa56bb0718a
bob <robert.d.borges@gmail.com>
2019-11-06 20:41:15 +0100

    added support for specifying target dirs by .txt file, allowing for operation on multiple files Also added fancy options menu.

--- elanize.sh
+++ elanize.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 # This script will check the given directory for a .wav file, then it will
-# create an elan file with associated with the .wav in the same directory.
+# create an elan file associated with the .wav in the same directory.
 # The .eaf file will have one tier and an anotation that occupies the entire
 # durration of the .wav file. If there is a .txt file in the directory with
 # the same name as the .wav file, the script will assume it is transcription
 # and add the contents of the .txt to the annotation.
 #
 # DEPENDENCIES: soxi, tidy
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Enter the directory you want to 'elanize'. Hint: use absolute paths"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo ""
-echo "Directory:" && read working_dir
-echo ""
+#
+ELANIZE () {
 for file in $working_dir/*.wav
 do
 	if [[ -f $file ]]; then
@@ -33,6 +29,93 @@ do
 		echo "$elan" | tidy -xml -iq - >> "$working_dir/$file_name.eaf" 
 	fi
 done
-echo "~~~~"
-echo "DONE"
-echo "~~~~"
+echo -e "\e[31m~~~~"
+echo -e "DONE"
+echo -e "~~~~\e[0m"
+}
+one_dir () {
+	echo "Please enter the directory you want to operate on."
+	echo "HINT: Use an absolute path."	
+	read working_dir
+	ELANIZE
+}
+two_dir () {
+	echo "Please enter a .txt file listing the directories you want to operate on."
+	echo "HINT: Use absoute paths to the file AND in the file."
+	read file_list
+	while read line; do
+		working_dir=$line
+		ELANIZE	
+	done < $file_list
+}
+help_msg () {
+	echo -e "\e[33mThis is a script to create an elan project from a .wav file."
+	echo -e "The .eaf file will have one tier and an anotation that occupies the entire"
+	echo -e "durration of the .wav file. If there is a .txt file in the directory with"
+	echo -e "the same name as the .wav file, the script will assume it is transcription"
+	echo -e "and add the contents of the .txt to the annotation."
+	echo -e ""
+	echo -e "Required DEPENDENCIES: soxi, tidy"
+	echo -e ""
+	echo -e "If tou choose to operate on one directory (Option 1), you must enter an"
+	echo -e "absolute path to that directory. Sommething like:\e[0m"
+	echo -e ""
+	echo -e "    /home/user/target_directory"
+	echo -e ""
+	echo -e "\e[33mIf you choose to operate on multiple directories, you must provide"
+	echo -e "a .txt file as input. An absolute path should be entered to that file, and"
+	echo -e "its contents should be a line by line list of directories to be operated"
+	echo -e "on by the script, also in the form of absolute paths."
+	echo -e ""
+	echo -e "OK?"
+	echo -e ""
+	sleep 1
+	echo -e "\e[31mNow repeating the options:"
+	echo -e "\e[33m"
+	PS3=""
+	sleep 1
+	echo "x" | select opt in "${options[@]}"; do break;done;  # $REPLY";;
+	export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+}
+
+echo -e "\e[31m |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+echo -e " | --- Welcome to elanize! --- |"
+echo -e " |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+sleep 1
+echo ""
+echo -e "How many directories to you want to operate on?\e[33m"
+echo ""
+sleep 1
+
+COLUMNS=12
+export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+options=("Work on a single directory" "Work on multiple directories" "Get help" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"Work on a single directory")
+			echo "You chose to work on ONE directory."
+			one_dir
+			break		
+			;;
+		"Work on multiple directories")
+			echo "You chose to work on MULTIPLE directories."
+			two_dir
+			break
+			;;
+		"Get help")
+			echo "So, you need help? Here it is:"
+			help_msg
+			;;
+		"Quit")
+			break
+			;;
+		*) 
+			PS3=""
+			echo -e "\e[1mInvalid option!!\e[0m Try again.\e[33m"
+			sleep 1
+			echo "x" | select opt in "${options[@]}"; do break;done;  # $REPLY";;
+			export PS3=$'\e[91mPlease \e[5menter \e[25myour choice:\e[0m '
+	esac
+done
+
