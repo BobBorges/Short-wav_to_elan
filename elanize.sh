1ac9ab5435cdb26973d9990a394908c9edee8076
bob <robert.d.borges@gmail.com>
2019-11-17 00:03:55 +0100

    fixed mistake since last version update and fixed formatting

--- elanize.sh
+++ elanize.sh
@@ -35,8 +35,8 @@ for d in $working_dir/*/; do
 		    sec_dur=$(soxi -D $file)					# this gets the durration of the audio file in seconds
 		    ms_dur_f=$(echo "${sec_dur}*1000"|bc)				# converts seconds to ms
 		    ms_dur=${ms_dur_f%.*}						# and removes the decimal and post decimal val
-		    if [[ -f "$working_dir/$file_name.txt" ]]; then			# checks if there's a .txt file by the same name as the .wav
-			    transcription=$( cat "$working_dir/$file_name.txt")	# assigns contents of .txt to a var
+		    if [[ -f "$d/$file_name.txt" ]]; then			# checks if there's a .txt file by the same name as the .wav
+			    transcription=$( cat "$d/$file_name.txt")	# assigns contents of .txt to a var
 		    fi
 		    # this is a non-pretty version of an empty elan file, filling in vars from the .wav and .txt
 		    elan="<?xml version=\"1.0\" encoding=\"UTF-8\"?><ANNOTATION_DOCUMENT AUTHOR=\"\" DATE=\"2019-11-05T14:16:24+01:00\" FORMAT=\"3.0\" VERSION=\"3.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.mpi.nl/tools/elan/EAFv3.0.xsd\"><HEADER MEDIA_FILE=\"\" TIME_UNITS=\"milliseconds\"><MEDIA_DESCRIPTOR MEDIA_URL=\"file://$file\" MIME_TYPE=\"audio/x-wav\" RELATIVE_MEDIA_URL=\"./$file_name.wav\"/><PROPERTY NAME=\"URN\">urn:nl-mpi-tools-elan-eaf:2bb01589-bf3d-4141-871d-8f31be10fd9b</PROPERTY><PROPERTY NAME=\"lastUsedAnnotationId\">1</PROPERTY></HEADER><TIME_ORDER><TIME_SLOT TIME_SLOT_ID=\"ts1\" TIME_VALUE=\"1\"/><TIME_SLOT TIME_SLOT_ID=\"ts2\" TIME_VALUE=\"$ms_dur\"/></TIME_ORDER><TIER LINGUISTIC_TYPE_REF=\"default-lt\" TIER_ID=\"transcription\"><ANNOTATION><ALIGNABLE_ANNOTATION ANNOTATION_ID=\"a1\" TIME_SLOT_REF1=\"ts1\" TIME_SLOT_REF2=\"ts2\"><ANNOTATION_VALUE>$transcription</ANNOTATION_VALUE></ALIGNABLE_ANNOTATION></ANNOTATION></TIER><LINGUISTIC_TYPE GRAPHIC_REFERENCES=\"false\" LINGUISTIC_TYPE_ID=\"default-lt\" TIME_ALIGNABLE=\"true\"/><CONSTRAINT DESCRIPTION=\"Time subdivision of parent annotation's time interval, no time gaps allowed within this interval\" STEREOTYPE=\"Time_Subdivision\"/><CONSTRAINT DESCRIPTION=\"Symbolic subdivision of a parent annotation. Annotations refering to the same parent are ordered\" STEREOTYPE=\"Symbolic_Subdivision\"/><CONSTRAINT DESCRIPTION=\"1-1 association with a parent annotation\" STEREOTYPE=\"Symbolic_Association\"/><CONSTRAINT DESCRIPTION=\"Time alignable annotations within the parent annotation's time interval, gaps are allowed\" STEREOTYPE=\"Included_In\"/></ANNOTATION_DOCUMENT>" 	
@@ -52,14 +52,16 @@ echo -e "DONE"
 echo -e "~~~~\e[0m"
 }
 one_dir () {
-	echo "Please enter the directory you want to operate on."
-	echo "HINT: Use an absolute path."	
+    echo -e ""
+	echo -e "\e[91mPlease \e[5menter \e[25mthe directory you want to operate on."
+	echo -e "\e[34mHINT: Use an absolute path.\e[0m"	
 	read working_dir
 	ELANIZE
 }
 two_dir () {
-	echo "Please enter a .txt file listing the directories you want to operate on."
-	echo "HINT: Use absoute paths to the file AND in the file."
+    echo -e ""
+	echo -e "\e[91mPlease \e[5menter \e[25ma .txt file listing the directories you want to operate on."
+	echo -e "\e[34mHINT: Use absoute paths to the file AND in the file.\e[0m"
 	read file_list
 	while read line; do
 		working_dir=$line
