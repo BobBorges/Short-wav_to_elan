3fa36cd7df11da01f942696750bb8a70031b2d48
bob <robert.d.borges@gmail.com>
2019-11-18 22:26:47 +0100

    fixed path vars to accommodate spaces

--- folder-and-elanize_wav_files.sh
+++ folder-and-elanize_wav_files.sh
@@ -31,36 +31,36 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 F_and_E_ize () {
-for file in $working_dir/*.wav
+for file in "${working_dir}"/*.wav
 do
-    if [[ -f $file ]]; then
-    echo "... putting the file --- $file --- into its folder ..."
-	file_bare=${file%.wav}
-	file_name=${file_bare##*/}
-	mkdir $working_dir/$file_name
-    sub_dir=$working_dir/$file_name
-    mv $file $sub_dir
+    if [[ -f "${file}" ]]; then
+        echo "... putting the file --- ${file} --- into its folder ..."
+	    file_bare=${file%.wav}
+	    file_name=${file_bare##*/}
+	    mkdir "${working_dir}"/$file_name
+        sub_dir="${working_dir}"/$file_name
+        mv "${file}" "${sub_dir}"
         if [[ -f "$file_bare.txt" ]]; then
             echo "... and putting --- $file_bare.txt --- in too ..."
-            mv "$file_bare.txt" $sub_dir
+            mv "$file_bare.txt" "${sub_dir}"
         fi
-    for file in $sub_dir/*.wav
+    for file in "${sub_dir}"/*.wav
     do
-        if [[ -f $file ]]; then
+        if [[ -f "${file}" ]]; then
             file_bare=${file%.wav}
             file_name=${file_bare##*/}
-            sec_dur=$(soxi -D $file)				# this gets the durration of the audio file in seconds
+            sec_dur=$(soxi -D "${file}")				# this gets the durration of the audio file in seconds
             ms_dur_f=$(echo "${sec_dur}*1000"|bc)		# converts seconds to ms
             ms_dur=${ms_dur_f%.*}				# and removes the decimal and post decimal val
-            if [[ -f "$sub_dir/$file_name.txt" ]]; then		# checks if there's a .txt file by the same name as the .wav
-                transcription=$( cat "$sub_dir/$file_name.txt")	# assigns contents of .txt to a var
+            if [[ -f "${sub_dir}/$file_name.txt" ]]; then		# checks if there's a .txt file by the same name as the .wav
+                transcription=$( cat "${sub_dir}/$file_name.txt")	# assigns contents of .txt to a var
             fi
             # this is a non-pretty version of an empty elan file, filling in vars from the .wav and .txt
-            elan="<?xml version=\"1.0\" encoding=\"UTF-8\"?><ANNOTATION_DOCUMENT AUTHOR=\"\" DATE=\"2019-11-05T14:16:24+01:00\" FORMAT=\"3.0\" VERSION=\"3.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.mpi.nl/tools/elan/EAFv3.0.xsd\"><HEADER MEDIA_FILE=\"\" TIME_UNITS=\"milliseconds\"><MEDIA_DESCRIPTOR MEDIA_URL=\"file://$file\" MIME_TYPE=\"audio/x-wav\" RELATIVE_MEDIA_URL=\"./$file_name.wav\"/><PROPERTY NAME=\"URN\">urn:nl-mpi-tools-elan-eaf:2bb01589-bf3d-4141-871d-8f31be10fd9b</PROPERTY><PROPERTY NAME=\"lastUsedAnnotationId\">1</PROPERTY></HEADER><TIME_ORDER><TIME_SLOT TIME_SLOT_ID=\"ts1\" TIME_VALUE=\"1\"/><TIME_SLOT TIME_SLOT_ID=\"ts2\" TIME_VALUE=\"$ms_dur\"/></TIME_ORDER><TIER LINGUISTIC_TYPE_REF=\"default-lt\" TIER_ID=\"transcription\"><ANNOTATION><ALIGNABLE_ANNOTATION ANNOTATION_ID=\"a1\" TIME_SLOT_REF1=\"ts1\" TIME_SLOT_REF2=\"ts2\"><ANNOTATION_VALUE>$transcription</ANNOTATION_VALUE></ALIGNABLE_ANNOTATION></ANNOTATION></TIER><LINGUISTIC_TYPE GRAPHIC_REFERENCES=\"false\" LINGUISTIC_TYPE_ID=\"default-lt\" TIME_ALIGNABLE=\"true\"/><CONSTRAINT DESCRIPTION=\"Time subdivision of parent annotation's time interval, no time gaps allowed within this interval\" STEREOTYPE=\"Time_Subdivision\"/><CONSTRAINT DESCRIPTION=\"Symbolic subdivision of a parent annotation. Annotations refering to the same parent are ordered\" STEREOTYPE=\"Symbolic_Subdivision\"/><CONSTRAINT DESCRIPTION=\"1-1 association with a parent annotation\" STEREOTYPE=\"Symbolic_Association\"/><CONSTRAINT DESCRIPTION=\"Time alignable annotations within the parent annotation's time interval, gaps are allowed\" STEREOTYPE=\"Included_In\"/></ANNOTATION_DOCUMENT>" 	
+            elan="<?xml version=\"1.0\" encoding=\"UTF-8\"?><ANNOTATION_DOCUMENT AUTHOR=\"\" DATE=\"2019-11-05T14:16:24+01:00\" FORMAT=\"3.0\" VERSION=\"3.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://www.mpi.nl/tools/elan/EAFv3.0.xsd\"><HEADER MEDIA_FILE=\"\" TIME_UNITS=\"milliseconds\"><MEDIA_DESCRIPTOR MEDIA_URL=\"file://${file}\" MIME_TYPE=\"audio/x-wav\" RELATIVE_MEDIA_URL=\"./$file_name.wav\"/><PROPERTY NAME=\"URN\">urn:nl-mpi-tools-elan-eaf:2bb01589-bf3d-4141-871d-8f31be10fd9b</PROPERTY><PROPERTY NAME=\"lastUsedAnnotationId\">1</PROPERTY></HEADER><TIME_ORDER><TIME_SLOT TIME_SLOT_ID=\"ts1\" TIME_VALUE=\"1\"/><TIME_SLOT TIME_SLOT_ID=\"ts2\" TIME_VALUE=\"$ms_dur\"/></TIME_ORDER><TIER LINGUISTIC_TYPE_REF=\"default-lt\" TIER_ID=\"transcription\"><ANNOTATION><ALIGNABLE_ANNOTATION ANNOTATION_ID=\"a1\" TIME_SLOT_REF1=\"ts1\" TIME_SLOT_REF2=\"ts2\"><ANNOTATION_VALUE>$transcription</ANNOTATION_VALUE></ALIGNABLE_ANNOTATION></ANNOTATION></TIER><LINGUISTIC_TYPE GRAPHIC_REFERENCES=\"false\" LINGUISTIC_TYPE_ID=\"default-lt\" TIME_ALIGNABLE=\"true\"/><CONSTRAINT DESCRIPTION=\"Time subdivision of parent annotation's time interval, no time gaps allowed within this interval\" STEREOTYPE=\"Time_Subdivision\"/><CONSTRAINT DESCRIPTION=\"Symbolic subdivision of a parent annotation. Annotations refering to the same parent are ordered\" STEREOTYPE=\"Symbolic_Subdivision\"/><CONSTRAINT DESCRIPTION=\"1-1 association with a parent annotation\" STEREOTYPE=\"Symbolic_Association\"/><CONSTRAINT DESCRIPTION=\"Time alignable annotations within the parent annotation's time interval, gaps are allowed\" STEREOTYPE=\"Included_In\"/></ANNOTATION_DOCUMENT>" 	
             echo "... ...creating --- $file_name.eaf --- ..."
-            touch "$sub_dir/$file_name.eaf"
+            touch "${sub_dir}/$file_name.eaf"
             # this puts the pretty-print version of the $elan to an .eaf file with the same name as the .wav
-            echo "$elan" | tidy -xml -iq - >> "$sub_dir/$file_name.eaf" 
+            echo "$elan" | tidy -xml -iq - >> "${sub_dir}/$file_name.eaf" 
         fi
     done
     fi
@@ -72,19 +72,19 @@ echo -e "~~~~\e[0m"
 one_dir () {
     echo -e ""
 	echo -e "\e[91mPlease \e[5menter \e[25mthe directory you want to operate on."
-	echo -e "\e[34mHINT: Use an absolute path.\e[0m"	
+	echo -e "\e[0m"	
 	read working_dir
 	F_and_E_ize
 }
 two_dir () {
     echo -e ""
 	echo -e "\e[91mPlease \e[5menter \e[25ma .txt file listing the directories you want to operate on."
-	echo -e "\e[34mHINT: Use absoute paths to the file AND in the file.\e[0m"
+	echo -e "\e[0m"
 	read file_list
 	while read line; do
-		working_dir=$line
+		working_dir="${line}"
 		F_and_E_ize	
-	done < $file_list
+	done < "${file_list}"
 }
 help_msg () {
 	echo -e "\e[33mThis is a script to put files .wav files within a directory into individual"
@@ -101,15 +101,16 @@ help_msg () {
 	echo -e ""
 	echo -e "Required DEPENDENCIES: soxi, tidy"
 	echo -e ""
-	echo -e "If tou choose to operate on one directory (Option 1), you must enter an"
-	echo -e "absolute path to that directory. Sommething like:\e[0m"
+	echo -e "If tou choose to operate on one directory (Option 1), you must enter a path"
+	echo -e "to that directory. Either use a path relative to the directory where the script"
+    echo -e "was initiated, or an absolute path. Sommething like:\e[0m"
 	echo -e ""
 	echo -e "    /home/user/target_directory"
 	echo -e ""
 	echo -e "\e[33mIf you choose to operate on multiple directories, you must provide"
-	echo -e "a .txt file as input. An absolute path should be entered to that fiel, and"
-	echo -e "its contents should be a line by line list of directories to be operated"
-	echo -e "on by the script."
+	echo -e "a .txt file as input. A path should be entered to that file, and its contents"
+	echo -e "should be a line by line list of directories to be operated contents on by the"
+    echo -e "script."
 	echo -e ""
 	echo -e "OK?"
 	echo -e ""
@@ -179,7 +180,6 @@ usage(){
     echo "  -i | --interactive      start program in interactive mode"
     echo "  -l | --list             pass target directories from .txt file"
     echo ""
-    echo "HINT: use absolute paths."
 }
 
 file_list=
@@ -197,8 +197,8 @@ while :; do
             exit
             ;;
         -l|--list)
-            if [ "$2" ];then
-                file_list=$2
+            if [ "${2}" ];then
+                file_list="${2}"
                 shift
             else
                 echo "-l takes an argument. Please try again."
@@ -211,13 +211,13 @@ while :; do
     shift
 done
 
-if [[ -f $file_list ]]; then
+if [[ -f "${file_list}" ]]; then
     while read line; do
-        working_dir=$line
+        working_dir="${line}"
         F_and_E_ize
-    done < $file_list
-elif [ "$1" ];then
-    working_dir=$1
+    done < "${file_list}"
+elif [ "${1}" ];then
+    working_dir="${1}"
     F_and_E_ize
 else
     echo ""
